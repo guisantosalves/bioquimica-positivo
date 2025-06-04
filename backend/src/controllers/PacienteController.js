b2ae067e396683f489c1eecfba5882e63e2490f2
MarcosIurak <iurak.gabriel@gmail.com>
2025-06-03 21:54:12 -0300

    Atualiza controller e service de paciente e modifica .env

--- backend/src/controllers/PacienteController.js
+++ backend/src/controllers/PacienteController.js
@@ -1,11 +1,28 @@
 import PacienteService from "../services/PacienteService.js";
+
 export default class PacienteController {
+
   static getPacientes(req, res) {
-    // some logic here
     PacienteService.getPaciente(req, res);
   }
 
+
+  static getPacienteById(req, res) {
+    PacienteService.getPacienteById(req, res);
+  }
+
+
   static postPaciente(req, res) {
     PacienteService.createPaciente(req, res);
+  }
+
+  
+  static updatePaciente(req, res) {
+    PacienteService.updatePaciente(req, res);
+  }
+
+  
+  static deletePaciente(req, res) {
+    PacienteService.deletePaciente(req, res);
   }
 }
